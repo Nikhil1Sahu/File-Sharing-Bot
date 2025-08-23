773ce3e3eacd5eca4dd7f0a60238c98629fac59d
Nikhil1Sahu <168706708+Nikhil1Sahu@users.noreply.github.com>
2025-08-24 03:03:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://nikhilsahu7j:dTQKfvo0jABO
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = os.environ.get("@network_of_kingdom")
+FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL")
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", "True")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "3"))
